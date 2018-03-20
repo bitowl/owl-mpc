72b09165dc3a62eb03cd0b594f75cc001c6553b5
bitowl <git@bitowl.de>
2018-03-20 09:56:42 +0100

    Try to reconnect if the MPD server is not running

--- extension/currently-playing.js
+++ extension/currently-playing.js
@@ -5,39 +5,50 @@ var MPC = require('mpc-js').MPC;
 module.exports = function (nodecg) {
     const mpcReplicant = nodecg.Replicant('mpc', {defaultValue:{
         song: '',
-        playing: false
+        playing: false,
+        connected: false,
     }, persistent: false});
 
     function retrieveCurrentSong() {
-        mpc.status.currentSong().then(song => {
-            mpcReplicant.value.artist = song.artist;
-            mpcReplicant.value.title = song.title;
-            mpcReplicant.value.playing = true;
+        mpc.status.status().then(status => { 
+            if (status.state == 'play') { 
+                mpc.status.currentSong().then(song => {
+                    mpcReplicant.value.artist = song.artist;
+                    mpcReplicant.value.title = song.title;
+                    mpcReplicant.value.playing = true;
+                });
+            } else {
+                mpcReplicant.value.playing = false;
+            }
         });
+    }
+
+    function reconnectToMpd() {
+        nodecg.log.info('Trying to reconnect to MDP server...');
+        connectToMpd();
     }
 
     function connectToMpd() {
         mpc.connectTCP('localhost', 6600).then(value => { 
             nodecg.log.info('Connected to MPD server');
+            mpcReplicant.connected = true;
             retrieveCurrentSong();
         }).catch(reason => {
+            mpc.disconnect();
             nodecg.log.error('Couldn\'t connect to MPD server', reason);
-            setTimeout(1000, function() {
-                nodecg.log.info('Trying to reconnect to MDP server...');
-                connectToMpd();
-            });
+            setTimeout(reconnectToMpd, 5000);
         });
     }
 
     var mpc = new MPC();
     mpc.on('changed-player', () => { 
-        mpc.status.status().then(status => { 
-            if (status.state == 'play') { 
-                retrieveCurrentSong();
-            } else {
-                mpcReplicant.value.playing = false;
-            }
-        });
+        retrieveCurrentSong();
+    });
+    mpc.on('socket-end', () => {
+        mpcReplicant.connected = false;
+        mpc.disconnect();
+        nodecg.log.error('MPD server closed connection');
+        reconnectToMpd();
     });
     connectToMpd();
     
