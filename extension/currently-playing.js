cfabfa6b13381e4f9353e94a5ea778b930be999e
bitowl <git@bitowl.de>
2019-09-03 23:17:06 +0200

    Fix mpc errors

--- extension/currently-playing.js
+++ extension/currently-playing.js
@@ -1,109 +1,128 @@
 'use strict';
 
-var MPC = require('mpc-js').MPC;
+const {MPC} = require('mpc-js');
 
 module.exports = function (nodecg) {
-    const mpcReplicant = nodecg.Replicant('mpc', {defaultValue:{
-        artist: 'not connected',
-        title: 'not connected'
-    }, persistent: false});
+	const mpcReplicant = nodecg.Replicant('mpc', {
+		defaultValue: {
+			artist: 'not connected',
+			title: 'not connected'
+		}, persistent: false
+	});
 
-    const connectedRepl = nodecg.Replicant('connected', {
-        defaultValue: false,
-        persistent: false
-    });
+	const connectedRepl = nodecg.Replicant('connected', {
+		defaultValue: false,
+		persistent: false
+	});
 
-    const playingRepl = nodecg.Replicant('playing', {
-        defaultValue: false,
-        persistent: false
-    });
+	const playingRepl = nodecg.Replicant('playing', {
+		defaultValue: false,
+		persistent: false
+	});
 
-    const volumeRepl = nodecg.Replicant('volume', {
-        defaultValue: 100,
-        persistent: true});
+	const volumeRepl = nodecg.Replicant('volume', {
+		defaultValue: 100,
+		persistent: true
+	});
 
-    function retrieveCurrentSong() {
-        mpc.status.status().then(status => { 
-            volumeRepl.value = status.volume;
+	let currentVolume = nodecg.readReplicant('volume');
 
-            if (status.state == 'play') { 
-                mpc.status.currentSong().then(song => {
-                    mpcReplicant.value.artist = song.artist;
-                    mpcReplicant.value.title = song.title;
-                    playingRepl.value = true;
-                });
-            } else {
-                playingRepl.value = false;
-            }
-        });
-    }
+	function retrieveCurrentSong() {
+		mpc.status.status().then(status => {
+			currentVolume = status.volume;
+			volumeRepl.value = status.volume;
 
-    function reconnectToMpd() {
-        nodecg.log.info('Trying to reconnect to MDP server...');
-        connectToMpd();
-    }
+			if (status.state === 'play') {
+				mpc.status.currentSong().then(song => {
+					mpcReplicant.value.artist = song.artist;
+					mpcReplicant.value.title = song.title;
+					playingRepl.value = true;
+				}).catch(errorHandler('play'));
+			} else {
+				playingRepl.value = false;
+			}
+		}).catch(errorHandler('status'));
+	}
 
-    function connectToMpd() {
-        mpc.connectTCP(nodecg.bundleConfig.host, nodecg.bundleConfig.port).then(value => { 
-            nodecg.log.info('Connected to MPD server');
-            connectedRepl.value = true;
-            retrieveCurrentSong();
-        }).catch(reason => {
-            mpc.disconnect();
-            nodecg.log.warn('Couldn\'t connect to MPD server', reason);
-            setTimeout(reconnectToMpd, nodecg.bundleConfig.reconnectTime);
-        });
-    }
+	function reconnectToMpd() {
+		nodecg.log.info('Trying to reconnect to MDP server...');
+		connectToMpd();
+	}
 
-    var mpc = new MPC();
-    mpc.on('changed-player', () => { 
-        retrieveCurrentSong();
-    });
-    mpc.on('socket-end', () => {
-        connectedRepl.value = false;
-        mpc.disconnect();
-        nodecg.log.error('MPD server closed connection');
-        reconnectToMpd();
-    });
-    connectToMpd();
-    
+	function connectToMpd() {
+		mpc.connectTCP(nodecg.bundleConfig.host, nodecg.bundleConfig.port).then(value => {
+			nodecg.log.info('Connected to MPD server');
+			connectedRepl.value = true;
+			retrieveCurrentSong();
+		}).catch(reason => {
+			mpc.disconnect();
+			nodecg.log.warn('Couldn\'t connect to MPD server', reason);
+			setTimeout(reconnectToMpd, nodecg.bundleConfig.reconnectTime);
+		});
+	}
 
-    nodecg.listenFor('play', function() {
-        mpc.playback.play();
-    });
+	const mpc = new MPC();
+	mpc.on('changed-player', () => {
+		retrieveCurrentSong();
+	});
+	mpc.on('socket-end', () => {
+		connectedRepl.value = false;
+		mpc.disconnect();
+		nodecg.log.error('MPD server closed connection');
+		reconnectToMpd();
+	});
+	connectToMpd();
 
-    nodecg.listenFor('pause', function() {
-        mpc.playback.pause();
-    });
+	nodecg.listenFor('play', () => {
+		mpc.playback.play().catch(errorHandler('play2'));
+	});
 
-    nodecg.listenFor('next', function() {
-        mpc.playback.next();
-    });
+	nodecg.listenFor('pause', () => {
+		mpc.playback.pause().catch(errorHandler('pause'));
+	});
 
-    nodecg.listenFor('ban', () => {
-        mpc.status.currentSong().then(song => {
-            mpc.storedPlaylists.listPlaylistInfo('stream').then(playlist => {
-                for (let i = 0; i < playlist.length; i++) {
-                    const element = playlist[i];
-                    if (element.path === song.path) {
-                        nodecg.log.info('Banned song ' + song.path);
-                        mpc.storedPlaylists.playlistDelete('stream', i);
-                        mpc.currentPlaylist.deleteId(song.id).catch(() => {
-                            nodecg.log.error('Could not delete song from current playlist');
-                        });
-                        mpc.playback.next();
-                        return;
-                    }
-                }
-                nodecg.log.error('Song ' + song.path + ' not found in playlist to ban');
-            });
-        }).catch(() => {
-            nodecg.log.error('No song playing to ban');
-        });
-    });
+	nodecg.listenFor('next', () => {
+		mpc.playback.next().catch(errorHandler('next'));
+	});
 
-    volumeRepl.on('change', volume => {
-        mpc.playbackOptions.setVolume(volume);
-    });
+	nodecg.listenFor('ban', () => {
+		mpc.status.currentSong().then(song => {
+			mpc.storedPlaylists.listPlaylistInfo('stream').then(playlist => {
+				for (let i = 0; i < playlist.length; i++) {
+					const element = playlist[i];
+					if (element.path === song.path) {
+						nodecg.log.info('Banned song ' + song.path);
+						mpc.storedPlaylists.playlistDelete('stream', i);
+						mpc.currentPlaylist.deleteId(song.id).catch(() => {
+							nodecg.log.error('Could not delete song from current playlist');
+						});
+						mpc.playback.next();
+						return;
+					}
+				}
 
+				nodecg.log.error('Song ' + song.path + ' not found in playlist to ban');
+			});
+		}).catch(() => {
+			nodecg.log.error('No song playing to ban');
+		});
+	});
+
+	volumeRepl.on('change', volume => {
+		if (isNaN(volume)) { // Volume is not yet set
+			return;
+		}
+
+		// Only call setVolume if the volume was changed by the user
+		if (volume !== currentVolume) {
+			mpc.playbackOptions.setVolume(volume).catch(errorHandler('setVolume'));
+		}
+	});
+
+	function errorHandler(name) {
+		return function (error) {
+			console.log(name);
+			console.trace(error);
+		};
+	}
 };
